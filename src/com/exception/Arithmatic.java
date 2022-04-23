5c3df64c516a554379379741daad47a7cdfbdb73
prakashkrish2022 <prakashmech925@gmail.com>
2022-04-22 18:15:35 -0700

    kit

--- src/com/exception/Arithmatic.java
+++ src/com/exception/Arithmatic.java
@@ -6,7 +6,7 @@ public class Arithmatic {
 System.out.println(1);
 	System.out.println(2);
 	System.out.println(3);
-	System.out.println(4);
+	System.out.println(44/0);
 	System.out.println(5/0);
 	System.out.println(6);
 	System.out.println(7);
